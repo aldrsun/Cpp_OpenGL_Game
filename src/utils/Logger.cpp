f7471d969c7e3dc385212befb56a17af5927b9cb
aldrsun <ali.dursun@metu.edu.tr>
2025-02-02 17:32:32 +0300

    Improvement on log system: Template added to handle different types

--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -1,49 +1,17 @@
 #include "Logger.h"
-#include <iostream>
 
-namespace Utils {
-    Logger::LogType Logger::m_logType = LogType::Console;
 
-    void Logger::SetLogType(const LogType &log_type) {
+namespace Utils::Logger {
+
+    void SetLogType(const LogType &log_type) {
         m_logType = log_type;
     }
 
-    void Logger::Log(const std::string& message, const LogLevel& log_level) {
-        std::string log_message;
-        switch (log_level) {
-            case LogLevel::Info:
-                log_message = "[INFO] " + message;
-                break;
-            case LogLevel::Warning:
-                log_message = "[WARNING] " + message;
-                break;
-            case LogLevel::Error:
-                log_message = "[ERROR] " + message;
-                break;
-            default:
-                log_message = "[UNSUPPORTED LOG LEVEL] " + message;
-                break;
-        }
-
-        switch (m_logType) {
-            case LogType::Console:
-                m_LogToConsole(log_message);
-                break;
-            case LogType::File:
-                m_LogToFile(log_message);
-                break;
-            case LogType::ConsoleAndFile:
-                m_LogToConsole(log_message);
-                m_LogToFile(log_message);
-                break;
-        }
-    }
-
-    void Logger::m_LogToConsole(const std::string &message) {
+    void m_LogToConsole(const char* message) {
         std::cout << message << std::endl;
     }
-    void Logger::m_LogToFile(const std::string &message) {
-        m_LogToConsole("[UNSUPPORTED LOG TYPE] " + message);
-    }
 
+    void m_LogToFile(const char* message) {
+        m_LogToConsole("[UNSUPPORTED LOG TYPE] ");
+    }
 } // namespace Utils
--- src/utils/Logger.h
+++ src/utils/Logger.h
@@ -1,31 +1,64 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include <string>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
 
-namespace Utils {
-    class Logger {
-    public:
-        enum class LogType {
-            Console,
-            File,
-            ConsoleAndFile,
-        };
-
-        enum class LogLevel {
-            Info,
-            Warning,
-            Error
-        };
-
-        static void SetLogType(const LogType& log_type);
-        static void Log(const std::string& message, const LogLevel& log_level = LogLevel::Info);
-
-    private:
-        static LogType m_logType;
-        static void m_LogToConsole(const std::string& message);
-        static void m_LogToFile(const std::string& message);
+namespace Utils::Logger {
+    enum class LogType {
+        Console,
+        File,
+        ConsoleAndFile,
     };
-} // Utils
+
+    enum class LogLevel {
+        Log,
+        Debug,
+        Info,
+        Warning,
+        Error
+    };
+
+    void SetLogType(const LogType& log_type);
+
+    inline std::unordered_map<LogLevel, const char*> LogLevelPrefix = {
+        {LogLevel::Log, "[LOG] "},
+        {LogLevel::Debug, "[DEBUG] "},
+        {LogLevel::Info, "[INFO] "},
+        {LogLevel::Warning, "[WARNING] "},
+        {LogLevel::Error, "[ERROR] "}
+    };
+
+    template<typename T>
+    void Log(const T& value, const LogLevel& log_level = LogLevel::Log) {
+        constexpr size_t BUFFER_SIZE = 256;
+        char buffer[BUFFER_SIZE];
+
+        char* ptr = buffer;
+        std::snprintf(ptr, BUFFER_SIZE, "%s", LogLevelPrefix[log_level]);
+        ptr += std::strlen(LogLevelPrefix[log_level]);
+
+        if constexpr (std::is_arithmetic_v<T>) {
+            std::snprintf(ptr, BUFFER_SIZE - (ptr - buffer), "%g", static_cast<double>(value));
+        } else if constexpr (std::is_same_v<T, std::string>) {
+            std::snprintf(ptr, BUFFER_SIZE - (ptr - buffer), "%s", value.c_str());
+        } else if constexpr (std::is_same_v<T, const char*>) {
+            std::snprintf(ptr, BUFFER_SIZE - (ptr - buffer), "%s", value);
+        } else {
+            std::ostringstream oss;
+            oss << value;
+            std::snprintf(ptr, BUFFER_SIZE - (ptr - buffer), "%s", oss.str().c_str());
+        }
+
+        std::cout << buffer << std::endl;
+    }
+
+    inline LogType m_logType = LogType::Console;
+
+    void m_LogToConsole(const char* message);
+    void m_LogToFile(const char* message);
+} // Utils::Logger
 
 #endif // LOGGER_H
