bdd8b47088ec4c517687f8dd081a79d250acd23c
aldrsun <ali.dursun@metu.edu.tr>
2025-02-25 22:09:31 +0300

    Improvement on log system: Variadic templates added

--- include/utils/Logger.h
+++ include/utils/Logger.h
@@ -6,59 +6,76 @@
 #include <sstream>
 #include <unordered_map>
 
-namespace Utils::Logger {
-    enum class LogType {
-        Console,
-        File,
-        ConsoleAndFile,
-    };
+namespace Utils {
 
-    enum class LogLevel {
-        Log,
-        Debug,
-        Info,
-        Warning,
-        Error
-    };
+    class Logger {
+    public:
+        enum class LogType {
+            Console,
+            File,
+            ConsoleAndFile,
+        };
+    
+        enum class LogLevel {
+            Log,
+            Debug,
+            Info,
+            Warning,
+            Error
+        };
 
-    void SetLogType(const LogType& log_type);
-
-    inline std::unordered_map<LogLevel, const char*> LogLevelPrefix = {
-        {LogLevel::Log, "[LOG] "},
-        {LogLevel::Debug, "[DEBUG] "},
-        {LogLevel::Info, "[INFO] "},
-        {LogLevel::Warning, "[WARNING] "},
-        {LogLevel::Error, "[ERROR] "}
-    };
-
-    template<typename T>
-    void Log(const T& value, const LogLevel& log_level = LogLevel::Log) {
-        constexpr size_t BUFFER_SIZE = 256;
-        char buffer[BUFFER_SIZE];
-
-        char* ptr = buffer;
-        std::snprintf(ptr, BUFFER_SIZE, "%s", LogLevelPrefix[log_level]);
-        ptr += std::strlen(LogLevelPrefix[log_level]);
-
-        if constexpr (std::is_arithmetic_v<T>) {
-            std::snprintf(ptr, BUFFER_SIZE - (ptr - buffer), "%g", static_cast<double>(value));
-        } else if constexpr (std::is_same_v<T, std::string>) {
-            std::snprintf(ptr, BUFFER_SIZE - (ptr - buffer), "%s", value.c_str());
-        } else if constexpr (std::is_same_v<T, const char*>) {
-            std::snprintf(ptr, BUFFER_SIZE - (ptr - buffer), "%s", value);
-        } else {
-            std::ostringstream oss;
-            oss << value;
-            std::snprintf(ptr, BUFFER_SIZE - (ptr - buffer), "%s", oss.str().c_str());
+        static void SetLogType(const LogType& log_type);
+        
+        template<typename ... Params>
+        static void Log(const LogLevel& log_level, const Params& ... params) {
+            char buffer[s_bufferSize];
+            char* p_cursor = buffer;
+            std::snprintf(p_cursor, s_bufferSize, "%s", m_LogLevelPrefix[log_level]);
+            p_cursor += std::strlen(m_LogLevelPrefix[log_level]);
+    
+            m_HandleLogParams(buffer, p_cursor, params...);
+    
+            std::cout << buffer << std::endl;
         }
 
-        std::cout << buffer << std::endl;
-    }
+        template<typename ... Params>
+        static void Log(const Params& ... params) {
+            Log(LogLevel::Log, params...);
+        }
 
-    inline LogType m_logType = LogType::Console;
+    private:
+        static std::unordered_map<LogLevel, const char*> m_LogLevelPrefix;
 
-    void m_LogToConsole(const char* message);
-    void m_LogToFile(const char* message);
+        static const size_t s_bufferSize = 256;
+        template<typename Param>
+        static void m_HandleLogParams(char* buffer, char* p_cursor, const Param& param) {
+            if constexpr (std::is_arithmetic_v<Param>) {
+                std::snprintf(p_cursor, s_bufferSize - (p_cursor - buffer), "%g", static_cast<double>(param));
+            } else if constexpr (std::is_same_v<Param, std::string>) {
+                std::snprintf(p_cursor, s_bufferSize - (p_cursor - buffer), "%s", param.c_str());
+            } else if constexpr (std::is_same_v<Param, const char*>) {
+                std::snprintf(p_cursor, s_bufferSize - (p_cursor - buffer), "%s", param);
+            } else {
+                std::ostringstream oss;
+                oss << param;
+                std::snprintf(p_cursor, s_bufferSize - (p_cursor - buffer), "%s", oss.str().c_str());
+            }
+        }
+    
+        template<typename Param, typename ... Params>
+        static void m_HandleLogParams(char* buffer, char* p_cursor, const Param& param, const Params& ... params) {
+            m_HandleLogParams(buffer, p_cursor, param);
+            p_cursor += std::strlen(p_cursor);
+    
+            *p_cursor = ' ';
+            p_cursor ++;
+            m_HandleLogParams(buffer, p_cursor, params ...);
+        }
+
+        static LogType s_logType; // TODO : WILL BE IMPROVED
+        static void m_LogToConsole(const char* message);
+        static void m_LogToFile(const char* message);
+    };
 } // Engine::Utils::Logger
 
 #endif // LOGGER_H
--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -1,17 +1,25 @@
 #include "utils/Logger.h"
 
+namespace Utils {
 
-namespace Utils::Logger {
+    std::unordered_map<Logger::LogLevel, const char*> Logger::m_LogLevelPrefix = {
+        {LogLevel::Log, "[LOG] "},
+        {LogLevel::Debug, "[DEBUG] "},
+        {LogLevel::Info, "[INFO] "},
+        {LogLevel::Warning, "[WARNING] "},
+        {LogLevel::Error, "[ERROR] "}
+    };
+    Logger::LogType Logger::s_logType = LogType::Console;
 
-    void SetLogType(const LogType &log_type) {
-        m_logType = log_type;
+    void Logger::SetLogType(const LogType &log_type) {
+        s_logType = log_type;
     }
 
-    void m_LogToConsole(const char* message) {
+    void Logger::m_LogToConsole(const char* message) {
         std::cout << message << std::endl;
     }
 
-    void m_LogToFile(const char* message) {
+    void Logger::m_LogToFile(const char* message) {
         m_LogToConsole("[UNSUPPORTED LOG TYPE] ");
     }
 } // namespace Engine::Utils::Logger
